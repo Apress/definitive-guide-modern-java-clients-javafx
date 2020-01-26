a87280364739e166191345263d3fbe876ae7c4e9
Paul Court <g@rgoyle.com>
2020-01-26 12:38:15 +0000

    Adds listing 3-1 for chapter 3.

---
+++ ch03-PropertiesBindings/propertiesexample/src/main/java/org/pmc/propertiesexample/App.java
@@ -0,0 +1,13 @@
+package org.pmc.propertiesexample;
+
+/**
+ *
+ * @author paul
+ */
+public class App {
+    private static IntegerProperty i1;
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
